5cde6655b1a57029ff16e5af2bdd533d93a0f4e2
ezrafrost7 <71141893+ezrafrost7@users.noreply.github.com>
2021-02-18 15:03:34 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace FrostCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Blockchain
+    {
+
+    }
+
+    class Block
+    {
+
+    }
+}
